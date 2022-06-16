27041ec3b2576cfaad5a0557b2ea350cf4c39e05
Brian <b.perez3237@gmail.com>
2022-06-16 19:18:54 -0400

    commit tree 498a095ff5ba6d4ba548bb362f5873a311187804

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,19 +1,25 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
 
+  def show
+    toy = Toy.find_by(id: params[:id])
+    render json: toy
+  end
+
   def index
     toys = Toy.all
     render json: toys
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy, status: :accepted
   end
 
   def destroy
